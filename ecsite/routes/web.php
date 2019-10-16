2bb05be0fc4ffb443c0d6cb93459d96d6e3d2760
kimura121212 <ykimuranoi@gmail.com>
2019-10-16 17:00:18 +0900

    カート追加機能実装

--- ecsite/routes/web.php
+++ ecsite/routes/web.php
@@ -20,3 +20,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'ItemController@index');
 Route::get('/item/{item}', 'ItemController@show');
+Route::post('/cartitem', 'CartItemController@store');
